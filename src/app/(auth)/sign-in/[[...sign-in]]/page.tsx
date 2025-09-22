72805da176ff4f2d14165ab4a7977eceab270df4
Vadym <nothing9537@gmail.com>
2025-09-21 21:53:27 -0500

    add test user

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/shared/ui/card';
+import { Separator } from '@/shared/ui/separator';
 import { ClerkLoaded, ClerkLoading, SignIn } from '@clerk/nextjs'
 import { Loader2 } from 'lucide-react'
 
@@ -5,7 +7,27 @@ const SignInPage = () => {
   return (
     <>
       <ClerkLoaded>
-        <SignIn path='/sign-in' />
+        <div>
+          <SignIn path='/sign-in' />
+          <Card className='mt-12'>
+            <CardHeader>
+              <CardTitle className='text-base'>
+                If you don&apos;t want to create your own account but still want to test the app, you can use this test account.
+              </CardTitle>
+            </CardHeader>
+            <Separator />
+            <CardContent>
+              <p>
+                Username: {' '}
+                <b>TestUser</b>
+              </p>
+              <p>
+                Password: {' '}
+                <b>GoodPasswordForTestUser123!</b>
+              </p>
+            </CardContent>
+          </Card>
+        </div>
       </ClerkLoaded>
       <ClerkLoading>
         <Loader2 className='animate-spin' />
